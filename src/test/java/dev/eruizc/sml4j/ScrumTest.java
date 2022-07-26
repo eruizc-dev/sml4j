666acce015287badfdfb26ec524f736669c6cab3
Emiliano Ruiz Carletti <contact@eruizc.dev>
2022-07-25 21:28:38 -0300

    Spaceees

--- src/test/java/dev/eruizc/sml4j/ScrumTest.java
+++ src/test/java/dev/eruizc/sml4j/ScrumTest.java
@@ -15,61 +15,61 @@ import org.junit.jupiter.api.*;
  * If rejected, goes back to wip
  * */
 public class ScrumTest {
-	StateMachine<Column, Actions> card;
+    StateMachine<Column, Actions> card;
 
-	static enum Column {
-		BACKLOG, WIP, REVIEW, DONE;
-	}
+    static enum Column {
+        BACKLOG, WIP, REVIEW, DONE;
+    }
 
-	static enum Actions {
-		START, REQUEST_REVIEW, APPROVE, REJECT;
-	}
+    static enum Actions {
+        START, REQUEST_REVIEW, APPROVE, REJECT;
+    }
 
-	@BeforeEach
-	void beforeEach() {
-		card = new StateMachine<>(
-				Column.BACKLOG,
-				new Transition<>(Column.BACKLOG, Actions.START, Column.WIP),
-				new Transition<>(Column.WIP, Actions.REQUEST_REVIEW, Column.REVIEW),
-				new Transition<>(Column.REVIEW, Actions.APPROVE, Column.DONE),
-				new Transition<>(Column.REVIEW, Actions.REJECT, Column.WIP)
-		);
-	}
+    @BeforeEach
+    void beforeEach() {
+        card = new StateMachine<>(
+                Column.BACKLOG,
+                new Transition<>(Column.BACKLOG, Actions.START, Column.WIP),
+                new Transition<>(Column.WIP, Actions.REQUEST_REVIEW, Column.REVIEW),
+                new Transition<>(Column.REVIEW, Actions.APPROVE, Column.DONE),
+                new Transition<>(Column.REVIEW, Actions.REJECT, Column.WIP)
+        );
+    }
 
-	@Test
-	void completeFlow() {
-		assertEquals(Column.BACKLOG, card.getState());
+    @Test
+    void completeFlow() {
+        assertEquals(Column.BACKLOG, card.getState());
 
-		card.transition(Actions.START);
-		assertEquals(Column.WIP, card.getState());
+        card.transition(Actions.START);
+        assertEquals(Column.WIP, card.getState());
 
-		card.transition(Actions.REQUEST_REVIEW);
-		assertEquals(Column.REVIEW, card.getState());
+        card.transition(Actions.REQUEST_REVIEW);
+        assertEquals(Column.REVIEW, card.getState());
 
-		card.transition(Actions.REJECT);
-		assertEquals(Column.WIP, card.getState());
+        card.transition(Actions.REJECT);
+        assertEquals(Column.WIP, card.getState());
 
-		card.transition(Actions.REQUEST_REVIEW);
-		assertEquals(Column.REVIEW, card.getState());
+        card.transition(Actions.REQUEST_REVIEW);
+        assertEquals(Column.REVIEW, card.getState());
 
-		card.transition(Actions.APPROVE);
-		assertEquals(Column.DONE, card.getState());
-	}
+        card.transition(Actions.APPROVE);
+        assertEquals(Column.DONE, card.getState());
+    }
 
-	@Test
-	void canBeRejectedManyTimes() {
-		card.transition(Actions.START);
-		card.transition(Actions.REQUEST_REVIEW);
+    @Test
+    void canBeRejectedManyTimes() {
+        card.transition(Actions.START);
+        card.transition(Actions.REQUEST_REVIEW);
 
-		for (int i = 0; i < 69; i++) {
-			card.transition(Actions.REJECT);
-			assertEquals(Column.WIP, card.getState());
+        for (int i = 0; i < 69; i++) {
+            card.transition(Actions.REJECT);
+            assertEquals(Column.WIP, card.getState());
 
-			card.transition(Actions.REQUEST_REVIEW);
-			assertEquals(Column.REVIEW, card.getState());
-		}
+            card.transition(Actions.REQUEST_REVIEW);
+            assertEquals(Column.REVIEW, card.getState());
+        }
 
-		card.transition(Actions.APPROVE);
-		assertEquals(Column.DONE, card.getState());
-	}
+        card.transition(Actions.APPROVE);
+        assertEquals(Column.DONE, card.getState());
+    }
 }

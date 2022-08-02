8cadbe5774bce723b0d8fe19ae3ec12ea8593e27
Emiliano Ruiz Carletti <contact@eruizc.dev>
2022-08-01 21:17:20 -0300

    Add javadoc

--- src/main/java/dev/eruizc/sml4j/StateMachine.java
+++ src/main/java/dev/eruizc/sml4j/StateMachine.java
@@ -2,6 +2,9 @@ package dev.eruizc.sml4j;
 
 import java.util.List;
 
+/**
+ * A finite State Machine
+ */
 public class StateMachine<S, A> {
 	private final List<Transition<S, A>> transitions;
 	private S state;
@@ -11,10 +14,18 @@ public class StateMachine<S, A> {
 		this.state = initialState;
 	}
 
+	/**
+	 * Gets the current state of the State Machine
+	 * @return current state
+	 */
 	public S getState() {
 		return this.state;
 	}
 
+	/**
+	 * @param action The action to be executed
+	 * @throws IllegalStateException Action cannot be executed in the current state
+	 */
 	public void transition(A action) {
 		var transition = transitions.stream()
 				.filter(t -> t.matches(state, action))
--- src/main/java/dev/eruizc/sml4j/StateMachineBuilder.java
+++ src/main/java/dev/eruizc/sml4j/StateMachineBuilder.java
@@ -2,21 +2,37 @@ package dev.eruizc.sml4j;
 
 import java.util.*;
 
+/**
+ * Builder for {@link StateMachine}
+ */
 public class StateMachineBuilder<S, A> {
 	private final List<Transition<S, A>> transitions = new LinkedList<>();
 	private S initialState;
 
+	/**
+	 * @param initialState The initial state for the State Machine
+	 * @return Builder
+	 */
 	public StateMachineBuilder<S, A> initialState(S initialState) {
 		this.initialState = initialState;
 		return this;
 	}
 
+	/**
+	 * @param from Initial state
+	 * @param action Allowed action given the initial state
+	 * @param to Resulting state
+	 * @return Builder
+	 */
 	public StateMachineBuilder<S, A> allowTransition(S from, A action, S to) {
 		var transition = new Transition<>(from, action, to);
 		transitions.add(transition);
 		return this;
 	}
 
+	/**
+	 * @return The built State Machine
+	 */
 	public StateMachine<S, A> build() {
 		return new StateMachine<S, A>(initialState, transitions);
 	}

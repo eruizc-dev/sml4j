b55ea0b1842af410ec1396ce14ed74e8856ea41d
Emiliano Ruiz Carletti <contact@eruizc.dev>
2023-02-09 21:19:03 -0300

    Fix tabs take 2

--- src/main/java/dev/eruizc/sml4j/IllegalTransitionException.java
+++ src/main/java/dev/eruizc/sml4j/IllegalTransitionException.java
@@ -1,20 +1,20 @@
 package dev.eruizc.sml4j;
 
 public class IllegalTransitionException extends Exception {
-    private Enum<?> state;
-    private Enum<?> action;
+	private Enum<?> state;
+	private Enum<?> action;
 
-    public IllegalTransitionException(Enum<?> state, Enum<?> action) {
-        super("Illegal state transition from state <" + state + "> with action <" + action + ">");
-        this.state = state;
-        this.action = action;
-    }
+	public IllegalTransitionException(Enum<?> state, Enum<?> action) {
+		super("Illegal state transition from state <" + state + "> with action <" + action + ">");
+		this.state = state;
+		this.action = action;
+	}
 
-    public Enum<?> getState() {
-        return this.state;
-    }
+	public Enum<?> getState() {
+		return this.state;
+	}
 
-    public Enum<?> getAction() {
-        return this.action;
-    }
+	public Enum<?> getAction() {
+		return this.action;
+	}
 }
